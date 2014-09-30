0aa0eba5431c71acfdeffa19bb86c96bd4efc764
Ashwin Srinath <ashwinsrnth@gmail.com>
2014-09-30 11:19:40 -0400

    Cleaned up gpuDA.py

--- gpuDA.py
+++ gpuDA.py
@@ -154,32 +154,3 @@ class GpuDA:
         # perform the copy:
         copier()
 
-if __name__ == "__main__":
-    
-    t1 = MPI.Wtime()
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-    
-    npx = 3
-    npy = 3
-    npz = 3
-        
-    assert(npx*npy*npz == size)
-
-    nx = 512
-    ny = 512
-    nz = 512
-
-    a = np.arange(nx*ny*nz, dtype=np.float64).reshape([nz, ny, nx])
-    a_gpu = gpuarray.to_gpu(a)
-    
-    comm = comm.Create_cart([npz, npy, npx], reorder=False)
-    da = GpuDA(comm, [nz, ny, nx], [npz, npy, npx], 1)
-    da.halo_swap(a_gpu)
-    t2 = MPI.Wtime()
-
-    if rank == 0:
-        print "Time taken: ", t2-t1
-
-    MPI.Finalize()
